ff00fa4e667e76413bf2505ff8c51a49bdbb54c6
geygrissom <greygrissom@me.com>
2020-10-29 22:04:55 -0700

    added test direcotry and first lines of code

---
+++ image_corrector.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3 
+import os
+from PIL import Image
+import sys
+
+# define global variables
+WORKING_DIR = os.paths.abspath(os.path.curdir)
+TARGET_DIR = ''
+def getImagePath(parent_directory):
+    target_directory = WORKING_DIR + parent_directory
+    return target_directory
+
+# open
+for img in getImagePath("images")
+# rotate
+PIL.Image.Image.rotate(90)
+# resize?
+PIL.Image.Image.resize((,))
+#save
+
+
+def
